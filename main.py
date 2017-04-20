bcdf788970f7382df687bf75b89495e7962ed856
StivenPeter <stivenpeter@gmail.com>
2017-04-20 12:28:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def minThresh(s):
+
+def lengthofs(s):
   u = len([letter for letter in s if letter.isupper()])
   l = len([letter for letter in s if letter.islower()])
   n = len([letter for letter in s if letter.isdigit()])
-  if u > 0 and l > 0 and n > 0: 
+  aln = len([letter for letter in s if letter.isalnum()])
+  return [u,l,n,an]
+
+def isminThresh(s):
+  res = lengthofs(s)
+  if res[0] > 0 and res[1] > 0 and res[2] > 0: 
     return True 
   return False 
   
